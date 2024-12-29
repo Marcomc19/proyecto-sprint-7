756ebe4bd110ac22219487c90103d89a654ecd1d
Marco Martinez <marcoamcontreras@gmail.com>
2024-12-29 10:04:03 -0600

    Se agregó una casilla de verificación para desplegar un c

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.write('Mi primera data app')
+st.title('Mi primera data app sobre :green[Venta de Automóviles]')
+st.header("por :blue[Marco Martínez] :sunglasses:", divider=True)
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
@@ -14,3 +15,20 @@ if hist_button: # al hacer clic en el botón
     fig = px.histogram(car_data, x="odometer")
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Construir gráfico de dispersión')
+#Se agrega un botón para crear el gráfico de dispersión solicitado
+
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    #Crea un gráfico de dispersión
+    fig = px.scatter(car_data, x = 'Odometer', y = 'price', title = 'Relación entre odómetro y precio')
+
+# crear una casilla de verificación
+build_bar = st.checkbox('Construir un histograma')
+
+if build_bar: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+    #Crear gráfica de barras
+    fig_bar = px.bar(car_data, x = 'model_year', y = 'odometer', title= 'Gráfica de Barras')
+    st.plotly_chart(fig_bar, use_container_width=True)
